f2ec377fe33be99eac0c7f79b39e81e9580570ac
Ken Qiu <kqiu@drmg.ca>
2018-09-05 11:18:16 -0400

    fix

--- controllers/order.js
+++ controllers/order.js
@@ -222,8 +222,8 @@ class Ctrl{
 		    	}  
 		        if (!error && response.statusCode == 200) {
 		            //console.log(body)
-		            var parser = require('xml2json')
-		            var json = JSON.parse(parser.toJson(body))
+		            var parser = require('xml2json-light')
+		            var json = JSON.parse(parser.xml2json(body))
 		            console.log(json)
 		            console.log(json.xml)
 		            resolve(json.xml)
